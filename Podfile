aa426267dd95abca5897ea1292fee78a5e1271ce
JiangXinxing <jxxjyh@163.com>
2016-06-04 23:34:33 +0800

    update third libraries.

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'QKFrame' do
 
 end
 
-pod 'Alamofire', '~> 3.3.1'
+pod 'Alamofire', '~> 3.4'
+pod 'AlamofireNetworkActivityIndicator', '~> 1.0'
+pod 'AlamofireImage', '~> 2.4'
 pod 'SwiftyJSON', '~> 2.3.2'
 pod 'MBProgressHUD', '~> 0.9'
--- QKFrame/QKLogger.swift
+++ QKFrame/QKLogger.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 class QKLogger {
-    private enum LEVEL: String {
+    
+    enum LEVEL: String {
         case DEBUG = "DEBUG"
         case INFO = "INFO"
         case WARN = "WARN"
@@ -17,26 +18,27 @@ class QKLogger {
     }
     
     class func debugLog<T>(object: T, fileName:NSString = #file, lineNumber: Int = #line, functionName: NSString = #function) {
-        let log = self.log(object, level: .DEBUG, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
+        let log = self.formate(object, level: .DEBUG, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
         NSLog(log)
     }
     
     class func infoLog<T>(object: T, fileName:NSString = #file, lineNumber: Int = #line, functionName: NSString = #function) {
-        let log = self.log(object, level: .INFO, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
+        let log = self.formate(object, level: .INFO, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
         NSLog(log)
     }
     
     class func warnLog<T>(object: T, fileName:NSString = #file, lineNumber: Int = #line, functionName: NSString = #function) {
-        let log = self.log(object, level: .WARN, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
+        let log = self.formate(object, level: .WARN, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
         NSLog(log)
     }
     
     class func errorLog<T>(object: T, fileName:NSString = #file, lineNumber: Int = #line, functionName: NSString = #function) {
-        let log = self.log(object, level: .ERROR, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
+        let log = self.formate(object, level: .ERROR, fileName: fileName, lineNumber: lineNumber, functionName: functionName)
         NSLog(log)
     }
     
-    private class func log<T>(object: T, level: LEVEL, fileName:NSString, lineNumber: Int, functionName: NSString) -> String {
+    /// Format output.
+    private class func formate<T>(object: T, level: LEVEL, fileName:NSString, lineNumber: Int, functionName: NSString) -> String {
         let logText = "[\(level.rawValue)][\(fileName.lastPathComponent) - \(functionName):\(lineNumber)] - \(object)"
         return logText
     }
